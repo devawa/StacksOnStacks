e35ecb86b1e6222ff4eeb3cb65408797ac4de288
Ntiko <ntikoclb@gmail.com>
2018-05-07 03:41:20 +0200

    Login Service class added

---
+++ Android_Dev/Tests/LoginService.java
@@ -0,0 +1,7 @@
+package com.example.ntikomathaba.arivllogindemo;
+
+class LoginService {
+    public boolean login(String username, String password) {
+        return "james".equals(username) && "bond".equals(password);
+    }
+}
