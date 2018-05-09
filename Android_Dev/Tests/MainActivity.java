0c03279d3bf920e29483e4598957e60928b4a40d
Ntiko <ntikoclb@gmail.com>
2018-05-09 15:36:03 +0200

    Updated MainActivity.java class

--- Android_Dev/Tests/MainActivity.java
+++ Android_Dev/Tests/MainActivity.java
@@ -1,11 +1,7 @@
-package com.example.ntikomathaba.arivllogindemo;
+package com.example.ntikomathaba.finallogin;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
-
 }
