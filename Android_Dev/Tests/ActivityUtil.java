cc850e007a922fcd604221c1a6db755bb9737e80
Ntiko <ntikoclb@gmail.com>
2018-05-09 15:31:37 +0200

    Updadted ActivityUtil.java class

--- Android_Dev/Tests/ActivityUtil.java
+++ Android_Dev/Tests/ActivityUtil.java
@@ -1,17 +1,18 @@
-package com.example.ntikomathaba.arivllogindemo;
+package com.example.ntikomathaba.finallogin;
 
 import android.content.Context;
 import android.content.Intent;
 
-
 public class ActivityUtil {
     private Context context;
 
-    public ActivityUtil(Context context){
+    public ActivityUtil(Context context) {
         this.context = context;
     }
 
-    public void startMainActivity(){
-        context.startActivity(new Intent(context,MainActivity.class));
+    public void startMainActivity() {
+        context.startActivity(new Intent(context, MainActivity.class));
     }
+
+
 }
