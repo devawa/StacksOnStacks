ac736788f626e5f4c580ea877941f8883c39d90e
LindaZwane <u14199468@tuks.co.za>
2018-07-19 18:36:01 +0200

    Update deleteUser.php

--- Web_Dev/deleteUser.php
+++ Web_Dev/deleteUser.php
@@ -1,19 +1,6 @@
 <?php
 
-	$host="localhost";
-	$username="root";
-	$password="arival123";
-	// Create a new connection and select database
-	$connection=new mysqli($host, $username, $password);
-	// Check if connected
-	// Might fail if password is incorrect, server is unreachable, etc
-	if($connection->connect_error)
-	die("Connection failure: ".$connection->connect_error);
-	else
-	{
-	$connection->select_db("arivldb");
-	echo"Connection success";
-	}
+	include 'dbconfig.php';
 	
 	function sanitise_input($data) {
 	$data = trim($data);
@@ -23,16 +10,14 @@
 	}
     
 	$phone = sanitise_input($_POST['phone']);
+	$active = 0;
 	
-	$query = "DELETE FROM `users` WHERE `users`.`phone` = '$phone'";
-	$result = $connection->query($query);
-
-	if($result)
-	{
-		echo "DELETED";
-		header("location:welcome.html");
-
-	}
+	$delete->execute();
 	
+	$delete->close();
+	$connection->close();
+
+	header('Location: welcome.html');
+
 	
 ?>
