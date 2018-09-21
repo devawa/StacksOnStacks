b0889a2aeafff08ea9692b16d06a53be66e1c095
Akua Afrane-Okese <36995926+AkuaB@users.noreply.github.com>
2018-09-21 10:47:41 +0200

    Add files via upload

--- Web_Dev/addUser.php
+++ Web_Dev/addUser.php
@@ -4,16 +4,23 @@
 	$name= $_POST['name'];
 	$surname= $_POST['surname'];
 	$id= $_POST['id'];
-	$idd=$_SESSION['id'];
 	$number= $_POST['number'];
 	$license = $_POST['license'];
 	$email =  $_POST['email'];
-	$password = "temp";
+	$password ;
 	$estate = $_POST['estate'];
 	
+	$alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+	for ($i = 0; $i < 8; $i++) {
+        $n = rand(0, strlen($alphabet)-1);
+        $pass[$i] = $alphabet[$n];
+	}
+	$password=implode("",$pass);
+	
+
 	$query = "insert into users 
-				(name,surname,number,license,email,active,guestRequest,resident,loggedOn,password,deleted)
-				VALUES ('$name','$surname','$number','$license','$email',0,0,1,0,'$password',0)"; 
+				(name,surname,number,idNumber,license,email,active,guestRequest,resident,loggedOn,password,deleted)
+				VALUES ('$name','$surname','$number','$id','$license','$email',1,0,1,0,'$password',0)"; 
 	if($result = $conn->query($query))
 	{
 		echo "inside 1"; 
@@ -31,7 +38,14 @@
 				$query = "insert into privileges (user_id,criteria,estate,activeEstate) values ($user_id, '$criteria','$estate',1)";
 				if($result = $conn->query($query)){
 				echo "Added to privileges"; 
-				header("Location:homePage.php");
+			
+
+				$msg = " Dear ".$name." ".$surname." \n Password = ".$password." Username = ".$name." \n Estate = ".$estate;
+				$header = "From: Arivl";
+				mail($email,"Registation",$msg,$header);
+				
+
+				header("Location:admin.php");
 			}else{
 				echo "didnt add";
 			}
@@ -40,4 +54,5 @@
 	else{
 		die ("something went wrong");
 	}
+
 ?>
